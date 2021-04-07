3ca8b40058ab4adfc12023c0afb5f9d0c2fb5624
Doreen Fan <dfan@lbl.gov>
2021-04-07 15:34:38 -0700

    Add maestro-related input parameters

--- Exec/reacting_tests/reacting_bubble/CastroMaestroInit.cpp
+++ Exec/reacting_tests/reacting_bubble/CastroMaestroInit.cpp
@@ -1,129 +1,19 @@
-#include <AMReX_VisMF.H>
+
+#include <Maestro_Data.H>
 #include <Castro.H>
 
 using namespace amrex;
-
-namespace {
-    const std::string level_prefix{"Level_"};
-}
 
 void Castro::MAESTRO_init ()
 {
     BL_PROFILE("Castro::MAESTRO_init()");
 
-    amrex::Print() << "Initialize from MAESTROeX plotfile " << MAESTRO_plotfile << "\n";
-
-    VisMF::IO_Buffer io_buffer(VisMF::GetIOBufferSize());
-
-    std::string line, word;
-    int step;
-
-    // Header
-    {
-        std::string File(MAESTRO_plotfile + "/Header");
-        Vector<char> fileCharPtr;
-        ParallelDescriptor::ReadAndBcastFile(File, fileCharPtr);
-        std::string fileCharPtrString(fileCharPtr.dataPtr());
-        std::istringstream is(fileCharPtrString, std::istringstream::in);
-
-        // read in title line
-        std::getline(is, line);
-
-        // read in time step number
-        is >> start_step;
-        GotoNextLine(is);
-        ++start_step;
-
-        // read in finest_level
-        is >> finest_level;
-        GotoNextLine(is);
-
-        // read in step
-        is >> step;
-        GotoNextLine(is);
-
-        // read in dt
-        is >> dt;
-        GotoNextLine(is);
-
-        // read in time
-        is >> t_old;
-        GotoNextLine(is);
-        t_new = t_old + dt;
-
-        // read in rel_eps
-        is >> rel_eps;
-        GotoNextLine(is);
-
-	// define MAESTROeX multifabs
-	Vector<MultiFab> sold(finest_level + 1);
-	Vector<MultiFab> uold(finest_level + 1);
-	
-        for (int lev = 0; lev <= finest_level; ++lev) {
-            // read in level 'lev' BoxArray from Header
-            BoxArray ba;
-            ba.readFrom(is);
-            GotoNextLine(is);
-
-            // create a distribution mapping
-            DistributionMapping dm{ba, ParallelDescriptor::NProcs()};
-
-            // set BoxArray grids and DistributionMapping dmap in AMReX_AmrMesh.H class
-            SetBoxArray(lev, ba);
-            SetDistributionMap(lev, dm);
-
-            // build MultiFab data
-            sold[lev].define(ba, dm, Nscal, ng_s);
-            uold[lev].define(ba, dm, AMREX_SPACEDIM, ng_s);
-            
-            // build FluxRegister data
-            if (lev > 0 && reflux_type == 2) {
-                flux_reg_s[lev] = std::make_unique<FluxRegister>(
-                    ba, dm, refRatio(lev - 1), lev, Nscal);
-            }
-        }
+    if (AMREX_SPACEDIM > 2) {
+	amrex::Abort("ERROR: Restart from MAESTROeX only works for DIM=2.");
     }
-
-    // read in the MultiFab data - put it in the "old" MultiFabs
-    for (int lev = 0; lev <= finest_level; ++lev) {
-        VisMF::Read(sold[lev], amrex::MultiFabFileFullPrefix(lev, MAESTRO_plotfile,
-                                                             level_prefix, "snew"));
-        VisMF::Read(uold[lev], amrex::MultiFabFileFullPrefix(lev, MAESTRO_plotfile,
-                                                             level_prefix, "unew"));
-    }
-
     
-    // model file (cell-centered)
-    {
-        std::string File(MAESTRO_modelfile);
-        Vector<char> fileCharPtr;
-        ParallelDescriptor::ReadAndBcastFile(File, fileCharPtr);
-        std::string fileCharPtrString(fileCharPtr.dataPtr());
-        std::istringstream is(fileCharPtrString, std::istringstream::in);
+    amrex::Print() << "Initialize from MAESTROeX plotfile " << maestrodata::maestro_plotfile << "\n";
 
-        // read in cell-centered base state
-        for (int i = 0; i < MAESTRO_npts_model; ++i) {
-            std::getline(is, line);
-            std::istringstream lis(line);
-            lis >> word;
-            rho0_old.array()(i) = std::stod(word);
-            lis >> word;
-            p0_old.array()(i) = std::stod(word);
-            lis >> word;
-            gamma1bar_old.array()(i) = std::stod(word);
-            lis >> word;
-            rhoh0_old.array()(i) = std::stod(word);
-            lis >> word;
-            beta0_old.array()(i) = std::stod(word);
-            lis >> word;
-            psi.array()(i) = std::stod(word);
-            lis >> word;
-            tempbar.array()(i) = std::stod(word);
-            lis >> word;
-            etarho_cc.array()(i) = std::stod(word);
-            lis >> word;
-            tempbar_init.array()(i) = std::stod(word);
-        }
-    }
+
 
 }
---
+++ Exec/reacting_tests/reacting_bubble/Maestro_Data.cpp
@@ -0,0 +1,183 @@
+#include <regex>
+
+#include <AMReX_ParmParse.H>
+#include <Maestro_Data.H>
+#include <Castro.H>
+
+using namespace amrex;
+
+/// Maestro parameter data
+std::string maestrodata::maestro_plotfile;
+std::string maestrodata::maestro_modelfile;
+AMREX_GPU_MANAGED int maestrodata::maestro_npts_model;
+std::string maestrodata::maestro_first_species;
+AMREX_GPU_MANAGED int maestrodata::maestro_nspec;
+AMREX_GPU_MANAGED amrex::Real maestrodata::maestro_cutoff_density;
+AMREX_GPU_MANAGED int maestrodata::maestro_init_type;
+AMREX_GPU_MANAGED bool maestrodata::maestro_spherical;
+
+//
+// Read in parameters from input file
+//
+void MaestroData::read_params() {
+        
+    // read in maestro-related input parameters
+    {
+    ParmParse pp("castro");
+#include <maestro_queries.H>
+    }
+    
+    // ERRORS: omitted necessary parameters
+    if (maestrodata::maestro_plotfile == "fillme") {
+        Abort("ERROR: MAESTRO_plotfile was not specified!\n");
+    }
+
+    if (maestrodata::maestro_first_species == "fillme") {
+        Abort("ERROR: MAESTRO_first_species was not specified!\n");
+    }
+
+    // WARNINGS: set parameters to default values
+    if (maestrodata::maestro_modelfile == "fillme") {
+        Print() << "WARNING: MAESTRO_modelfile was not specified!\n"
+		<< "Setting modelfile to default: "
+		<< maestrodata::maestro_plotfile << "/BaseCC_0 \n";
+	maestrodata::maestro_modelfile =
+	    maestrodata::maestro_plotfile + "/BaseCC_0";
+    }
+    
+    if (maestrodata::maestro_nspec == 0) {
+        Print() << "WARNING: MAESTRO_nspec was not specified!\n"
+		<< "Setting nspec to default: " << NumSpec << "\n";
+	maestrodata::maestro_nspec = NumSpec;
+    }
+    
+}
+
+//
+// Initialize Maestro data
+// 
+void MaestroData::setup() {
+    /*
+    // set input parameters
+    pltfile = new amrex::PlotFileData(maestrodata::maestro_plotfile);
+    finest_level = pltfile->finestLevel();
+
+    const auto probLo = pltfile->probLo();
+    const auto probHi = pltfile->probHi();
+
+    grid.resize(finest_level + 1);
+    dmap.resize(finest_level + 1);
+    geom.resize(finest_level + 1);
+    for (int lev = 0; lev <= finest_level; ++lev) {
+        grid[lev] = pltfile->boxArray(lev);
+        dmap[lev] = pltfile->DistributionMap(lev);
+
+        Box domain = pltfile->probDomain(lev);
+        RealBox real_box(probLo, probHi);
+        geom[lev].define(domain, &real_box);
+    }
+
+    // setup geometry
+    const auto domainBoxFine = pltfile->probDomain(finest_level);
+    const auto dxFine = pltfile->cellSize(finest_level);
+
+    // read input MultiFabs
+    p0_mf.resize(finest_level + 1);
+    temp_mf.resize(finest_level + 1);
+    for (int lev = 0; lev <= finest_level; ++lev) {
+	if (maestrodata::maestro_init_type == 1) {
+	    p0_mf[lev] = pltfile->get(lev, "p0");
+	} else {
+	    p0_mf[lev] = pltfile->get(lev, "p0pluspi");
+	}
+	temp_mf[lev] = pltfile->get(lev, "tfromp");
+    }
+
+    // Note: state stores (rho, rhoh, X_k)
+    state_mf.resize(finest_level + 1);
+    u_mf.resize(finest_level + 1);
+    w0_mf.resize(finest_level + 1);
+    for (int lev = 0; lev <= finest_level; ++lev) {
+	state_mf[lev].define(grid[lev], dmap[lev], 2 + maestrodata::maestro_nspec, 0);
+        u_mf[lev].define(grid[lev], dmap[lev], AMREX_SPACEDIM, 0);
+        w0_mf[lev].define(grid[lev], dmap[lev], AMREX_SPACEDIM, 0);
+    }
+
+    // full states
+    for (int lev = 0; lev <= finest_level; ++lev) {
+	MultiFab::Copy(state_mf[lev], pltfile->get(lev, "rho"), 0, 0, 1, 0);
+	MultiFab::Copy(state_mf[lev], pltfile->get(lev, "rhoh"), 0, 1, 1, 0);
+	for (int i = 0; i < maestrodata::maestro_nspec; ++i) {
+	    // get X for all species
+	}
+    }
+    
+    // velocities
+    for (int i = 0; i < AMREX_SPACEDIM; ++i) {
+        std::string x = "vel";
+        std::string w = "w0";
+        x += (120 + i);
+        w += (120 + i);
+
+        for (int lev = 0; lev <= finest_level; ++lev) {
+            MultiFab::Copy(u_mf[lev], pltfile->get(lev, x), 0, i, 1, 0);
+            MultiFab::Copy(w0_mf[lev], pltfile->get(lev, w), 0, i, 1, 0);
+        }
+    }
+
+
+    // model file (cell-centered)
+    std::string line, word;
+    int npts_model = maestrodata::maestro_npts_model;
+    r_cc_loc.resize(npts_model);
+    rho0.resize(npts_model);
+    rhoh0.resize(npts_model);
+    p0.resize(npts_model);
+    gamma1bar.resize(npts_model);
+    tempbar.resize(npts_model);
+    
+    {
+        std::string File(maestrodata::maestro_modelfile);
+        Vector<char> fileCharPtr;
+        ParallelDescriptor::ReadAndBcastFile(File, fileCharPtr);
+        std::string fileCharPtrString(fileCharPtr.dataPtr());
+        std::istringstream is(fileCharPtrString, std::istringstream::in);
+
+        // read in cell-centered base state
+        for (int i = 0; i < npts_model; ++i) {
+            std::getline(is, line);
+            std::istringstream lis(line);
+            lis >> word;
+	    r_cc_loc[i] = std::stod(word);
+	    lis >> word;
+            rho0[i] = std::stod(word);
+            lis >> word;
+            rhoh0[i] = std::stod(word);
+            lis >> word;
+            p0[i] = std::stod(word);
+            lis >> word;
+            gamma1bar[i] = std::stod(word);
+            lis >> word;
+            tempbar[i] = std::stod(word);
+        }
+    }
+    */
+}
+
+//
+// Initialize Castro data using Maestro data
+//
+void MaestroData::init() {
+    
+    
+}
+
+
+//
+// Test case: read in Maestro data and output on Castro grid
+//
+void MaestroData::test() {
+    
+    
+}
+
